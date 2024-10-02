342607f59f587e866a94d6a23853c5f37f8ca66a
Manohardev123 <mjjobs2338@gmail.com>
2024-10-02 18:46:53 +0530

    Update Modifying.py

--- Modifying.py
+++ Modifying.py
@@ -3,3 +3,15 @@ arr = [10, 20, 30, 40, 50]
 # Modifying the second element
 arr[1] = 25
 print("Modified Array:", arr)
+
+
+
+arr = [10, 20, 30]
+
+# Appending a single element
+arr.append(40)
+print("After appending:", arr)
+
+# Adding multiple elements
+arr.extend([50, 60])
+print("After extending:", arr)
